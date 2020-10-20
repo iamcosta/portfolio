f466be5587367c303831cc88b01a013cfa8b74c2
02234595266 <iambarrosodacosta@live.com>
2020-10-20 09:11:31 -0400

    adicao de link para site de ícones utilizados

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className='footer-general'>
             <div className='footer-container'>
-                <p className='footer-content'>Por Iam Barroso Ⓒ 2019</p>
+                <p className='footer-content'>Por Iam Barroso Ⓒ 2019 | <a href="https://www.flaticon.com/" target="__blank">Ícones</a></p>
             </div>
         </div>
     )
